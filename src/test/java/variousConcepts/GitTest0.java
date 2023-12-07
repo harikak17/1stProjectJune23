55857fe1778a07f802327ad7f8a585ee628c178d
harikak17 <127039137+harikak17@users.noreply.github.com>
2023-12-07 12:53:09 -0600

    2nd commit

---
+++ src/test/java/variousConcepts/GitTest0.java
@@ -0,0 +1,13 @@
+package variousConcepts;
+
+public class GitTest0 {
+
+	public static void main(String[] args) {
+		
+
+	}
+	public static void printme1(){
+		System.out.println("GitTest0"); 
+	}
+
+}
